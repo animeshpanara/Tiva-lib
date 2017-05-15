dd0f57c651bc27b4114d6c69e83628725be40eb2
animeshpanara <animeshpanara30@gmail.com>
2017-05-15 11:19:21 +0530

    Update main1.c

--- lower-control-system-1.0 with odometry/main1.c
+++ lower-control-system-1.0 with odometry/main1.c
@@ -8,62 +8,25 @@
 #include "userLib/init.h"
 #include "userLib/pidController.h"
 #include "userLib/movingArray.h"
-
+#include "userLib/odometry.h"
 #define KP_motorA 0.05
 #define KI_motorA 0.0
 #define KD_motorA 0.3
 #define KP_motorB 0.05
 #define KI_motorB 0.0
 #define KD_motorB 0.3
-#define pi 3.14159265
+
+
 
 volatile float desiredRPM[2] = {0.0,0.0}, currentRPM[2] = {0.0,0.0}, out[2] = {0.0,0.0},bufferDesiredRPM[2] = {0.0,0.0};
-float distance[2]={0.0,0.0};
-float position[3]={0.0,0.0,0.0};
-
+float Position[3]={0};
 int uartReceiveCount = 0;
 long int count[2]={0},prev[2]={0};
 int32_t maxPWM = 10;
 int32_t minPWM = 0;
 float Dc=0;
 
-float getX(float DC)
-{
-	return position[X]+DC*cos((position[Phi]));//(pi*position[Phi])/180.0);
-}
-float getY(float DC)
-{
-	return position[Y]+DC*sin((position[Phi]));
-}
-float getPhi()
-{
-	return position[Phi]+(distance[A]-distance[B])/(DistanceBetweenWheels);
-}
-float getDc()
-{
-	distance[A]=(2*pi*RADIUSA*GetTicks(A))/NumberOfTicksPerRevolution;
-	distance[B]=(2*pi*RADIUSB*GetTicks(B))/NumberOfTicksPerRevolution;
-	return 	(distance[A]+distance[B])/2.0;
-}
-long int GetTicks(int i)
-{
-	int x,y;
-	if(i==A)
-	{
-		count[A]= QEIPositionGet(QEI0_BASE);
-		x= count[A]-prev[A];
-		prev[A]=count[A];
-		return x;
-	}
-	if(i==B)
-	{
-		count[B]=QEIPositionGet(QEI1_BASE);
-		x=(count[B]-prev[B]);
-		prev[B]=count[B];
-		return -x;
-	}
 
-}
 int main() {
 	SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 	uart0Init();
@@ -94,15 +57,15 @@ int main() {
 void Timer0IntHandler(void) {
 		TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
 	    Dc=getDc();
-	    position[X]=getX(Dc);
-	    position[Y]=getY(Dc);
-	    position[Phi]=getPhi();
+	    Position[X]=getX(Dc);
+	    Position[Y]=getY(Dc);
+	    Position[Phi]=getPhi();
 	   	UARTCharPut(UART0_BASE,' ');
-	    UART_OutDec(10*position[X],0);
+	    UART_OutDec(10*Position[X],0);
 	    UARTCharPut(UART0_BASE,' ');
-	    UART_OutDec(10*position[Y],0);
+	    UART_OutDec(10*Position[Y],0);
 	    UARTCharPut(UART0_BASE,' ');
-	    UART_OutDec(180*position[Phi]/pi,0);
+	    UART_OutDec(180*Position[Phi]/pi,0);
 
 
 	 	UARTCharPut(UART0_BASE,0x0d);
@@ -151,4 +114,23 @@ void QEI1IntHandler(void) {
 		out[B] = 0;
 	}
 	setPWM(out[B],B);
+}
+long int GetTicks(int i)
+{
+    int x;
+    if(i==A)
+    {
+        count[A]= QEIPositionGet(QEI0_BASE);//configure qei0 first with max ticks as 0xffffffff;
+        x= count[A]-prev[A];
+        prev[A]=count[A];
+        return x;
+    }
+    if(i==B)
+    {
+        count[B]=QEIPositionGet(QEI1_BASE);//configure qei1 first with max ticks as 0xffffffff;
+        x=(count[B]-prev[B]);
+        prev[B]=count[B];
+        return x; //-x  if ur taking ticks of qei1 reverse or negative of direction of qei0 else x
+    }
+
 }
